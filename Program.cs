e519fe1bebe5853600c71a8b053c47b3df2ccadf
airsicktitan <adamcwhooper@yahoo.com>
2024-12-31 22:17:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,193 @@
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        public enum Shapes
+        {
+            X,
+            O
+        }
+
+        public static bool player1Turn { get; set; }
+        public static bool player2Turn { get; set; }
+        static void Main(string[] args)
+        {
+            string[,] squares =
+            {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+            };
+
+            Console.WriteLine($"Player One: {Shapes.X}\nPlayer Two: {Shapes.O}\n\n");
+            DrawBoard(squares);
+            Console.WriteLine("Player One goes first\n");
+            player1Turn = true;
+            player2Turn = false;
+
+            bool winner = false;
+
+            while (!winner)
+            {
+                Console.WriteLine(player1Turn
+                    ? "Player 1, choose a square (1-9):"
+                    : "Player 2, choose a square (1-9):");
+
+                _ = int.TryParse(Console.ReadLine(), out int playerMove);
+
+                switch (playerMove)
+                {
+                    case 1:
+                        if (squares[0, 0] == "X" || squares[0, 0] == "O") Console.WriteLine("Square is already taken");
+                        else squares[0, 0] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 2:
+                        if (squares[0, 1] == "X" || squares[0, 1] == "O") Console.WriteLine("Square is already taken");
+                        else squares[0, 1] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 3:
+                        if (squares[0, 2] == "X" || squares[0, 2] == "O") Console.WriteLine("Square is already taken");
+                        else squares[0, 2] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 4:
+                        if (squares[1, 0] == "X" || squares[1, 0] == "O") Console.WriteLine("Square is already taken");
+                        else squares[1, 0] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 5:
+                        if (squares[1, 1] == "X" || squares[1, 1] == "O") Console.WriteLine("Square is already taken");
+                        else squares[1, 1] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 6:
+                        if (squares[1, 2] == "X" || squares[1, 2] == "O") Console.WriteLine("Square is already taken");
+                        else squares[1, 2] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 7:
+                        if (squares[2, 0] == "X" || squares[2, 0] == "O") Console.WriteLine("Square is already taken");
+                        else squares[2, 0] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 8:
+                        if (squares[2, 1] == "X" || squares[2, 1] == "O") Console.WriteLine("Square is already taken");
+                        else squares[2, 1] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+                    case 9:
+                        if (squares[2, 2] == "X" || squares[2, 2] == "O") Console.WriteLine("Square is already taken");
+                        else squares[2, 2] = PlayerMove();
+
+                        DrawBoard(squares);
+                        break;
+
+                    default:
+                        Console.WriteLine("Square not valid");
+                        break;
+                }
+
+                if (CheckWinner(squares))
+                {
+                    string winnerName = player1Turn ? "Player 2" : "Player 1";
+                    Console.WriteLine($"{winnerName} has won the game!");
+                    break;
+                }
+            }
+
+        }
+
+        private static void DrawBoard(string[,] squares)
+        {
+            for (int i = 0; i < squares.GetLength(0); i++)
+            {
+                for (int j = 0; j < squares.GetLength(1); j++)
+                {
+                    Console.Write($"{squares[i, j]}");
+                    if (j < squares.GetLength(1) - 1)
+                        Console.Write(" | ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        private static string PlayerMove()
+        {
+            if (player1Turn)
+            {
+                (player1Turn, player2Turn) = PlayerTurn(player1Turn, player2Turn);
+                return Shapes.X.ToString();
+            }
+            else
+            {
+                (player1Turn, player2Turn) = PlayerTurn(player1Turn, player2Turn);
+                return Shapes.O.ToString();
+            }
+        }
+
+        private static (bool, bool) PlayerTurn(bool player1Turn, bool player2Turn)
+        {
+            if(player1Turn)
+            {
+                player1Turn = false;
+                player2Turn = true;
+            }
+            else
+            {
+                player1Turn = true;
+                player2Turn = false;
+            }
+
+            return (player1Turn, player2Turn);
+        }
+
+        private static bool CheckWinner(string[,] shapes)
+        {
+            // Rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (shapes[i, 0] == shapes[i, 1] && shapes[i, 1] == shapes[i, 2])
+                {
+                    if (shapes[i, 0] == "X" || shapes[i, 0] == "O")
+                        return true;
+                }
+            }
+
+            // Columns
+            for (int j = 0; j < 3; j++)
+            {
+                if (shapes[0, j] == shapes[1, j] && shapes[1, j] == shapes[2, j])
+                {
+                    if (shapes[0, j] == "X" || shapes[0, j] == "O")
+                        return true;
+                }
+            }
+
+            // Diagonal (top-left to bottom-right)
+            if (shapes[0, 0] == shapes[1, 1] && shapes[1, 1] == shapes[2, 2])
+            {
+                if (shapes[0, 0] == "X" || shapes[0, 0] == "O")
+                    return true;
+            }
+
+            // Diagonal (top-right to bottom-left)
+            if (shapes[0, 2] == shapes[1, 1] && shapes[1, 1] == shapes[2, 0])
+            {
+                if (shapes[0, 2] == "X" || shapes[0, 2] == "O")
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
